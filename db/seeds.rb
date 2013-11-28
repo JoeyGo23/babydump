875af68c9ab8387c03bec8af7db5bd6eeaa89c91
Vincent Cabansag <vcabansag@gmail.com>
2013-11-27 18:26:20 -0600

    Added a method to display a Baby's full_name.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,8 @@
-Baby.create first_name: 'Elliott', last_name: 'Revitte', dob: Date.parse('August 14, 2011')
-Baby.create first_name: 'Caleb', last_name: 'Warstler', dob: Date.parse('August 12, 2013')
+Baby.destroy_all
+puts 'Deleted all babies.'
+
+elliott = Baby.create first_name: 'Elliott', last_name: 'Revitte', dob: Date.parse('August 14, 2011')
+puts "A new baby has been born! #{Baby.last.full_name}."
+
+caleb = Baby.create first_name: 'Caleb', last_name: 'Warstler', dob: Date.parse('August 12, 2013')
+puts "A new baby has been born! #{Baby.last.full_name}."
