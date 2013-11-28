514b5f8cc4d15e2e44f053f1c981a806b46f973a
Vincent Cabansag <vcabansag@gmail.com>
2013-11-28 01:41:35 -0600

    User can upload a photo.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,18 @@
 class PhotosController < ApplicationController
 
   def new
+    @baby = Baby.find_by id: params[:baby_id]
+    @photo = @baby.photos.build
+  end
+
+  def create
+    @baby = Baby.find_by id: params[:baby_id]
+    @photo = @baby.photos.build
+    @photo.photo = params[:photo][:photo]
+    @photo.baby_id = params[:baby_id]
+    if @photo.save
+      redirect_to baby_path(@baby)
+    end
   end
 
 end
