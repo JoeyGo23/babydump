ee0bb49d931c2b3c87853a7ace367ac8c31fdb77
Vincent Cabansag <vcabansag@gmail.com>
2013-11-28 10:13:23 -0600

    Setting up CarrierWave to crop images.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,12 @@
 class Photo < ActiveRecord::Base
+  include CarrierWave::MiniMagick
+
+  process resize_to_fit: [800, 800]
+
+  version :thumb do
+    process resize_to_fill: [200,200]
+  end
+
   mount_uploader :photo, PhotoUploader
 
   belongs_to :baby
