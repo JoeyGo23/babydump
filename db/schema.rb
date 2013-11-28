86f6403491ee83cb3d3883c5dc134f49fa1b5210
Vincent Cabansag <vcabansag@gmail.com>
2013-11-28 00:19:38 -0600

    Created Photo model with baby_id and photo columns.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131128000612) do
+ActiveRecord::Schema.define(version: 20131128061906) do
 
   create_table "babies", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.date     "dob"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "photos", force: true do |t|
+    t.integer  "baby_id"
+    t.string   "photo"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
