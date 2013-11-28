58a8d6d810e980cdbfdeaffec63e3e217e7ed14b
Vincent Cabansag <vcabansag@gmail.com>
2013-11-27 18:01:18 -0600

    Initial commit. Rails 4 stock app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Babydump::Application.config.secret_key_base = '1a58edfe60019f8260c45c7c97717e8d533995e7e564f97c641a269fcd736006e3b806b3e9ae055e56aba3bd72e23cfa207386353c08cef82dc18b17de228ba7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Babydump::Application.config.session_store :cookie_store, key: '_babydump_session'
